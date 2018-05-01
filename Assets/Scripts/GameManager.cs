599ef712419c55a0ee2916fe4803bb2ad720cf74
moon <moonsin@outlook.com>
2018-05-01 16:02:14 +0100

    开场动画

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	private StartStory startStoryScript;
+
+	void Awake(){
+
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this) {
+			Destroy (gameObject);
+		}
+
+		startStoryScript = GetComponent<StartStory>();
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Story/StartStory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartStory : MonoBehaviour {
+
+	public GameObject beginingStoryCanvas;
+	private Text legend;
+	private Text story;
+	private bool Showstory = false;
+	private float transparency = 0f;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (beginingStoryCanvas);
+		legend = GameObject.Find("legend").GetComponent<Text>();
+		story = GameObject.Find("Story").GetComponent<Text>();
+		legend.enabled = false;
+		story.enabled = false;
+
+		legend.color = new Color (255, 0, 0, transparency);
+		legend.enabled = true;
+		Showstory = true;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Showstory) {
+			transparency += 0.005f;
+			legend.color = new Color (255, 0, 0, transparency);
+			if (transparency == 1f) {
+				Showstory = false;
+			}
+		}
+	}
+}
